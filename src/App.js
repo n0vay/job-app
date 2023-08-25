b7c7ad0e7d11eda6b5b2ed26ac285500c4a9218a
n0vay <koshantanu@gmail.com>
2023-08-25 00:02:20 -0400

    created Browser router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import {createBrowserRouter,Route,createRoutesFromElements,RouterProvider,} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
+
+//Layouts
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path = "/" element = {<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Route>
+  )
+);
+
 function App() {
   return (
-    <BrowserRouter>
-    <header>  
-      <nav>
-        <h1> Job Router </h1>
-        <NavLink to="/"> Home </NavLink>
-        <NavLink to = "about"> About </NavLink>
-      </nav>
-    </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   );
 }
 
