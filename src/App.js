0537f8fe04d9a563acf2f50d4ab02873712d7b4c
n0vay <koshantanu@gmail.com>
2023-10-24 03:04:30 -0400

    added careers page

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ import About from "./pages/About";
 //Layouts
 import RootLayout from "./layouts/RootLayout";
 import HelpLayout from "./layouts/HelpLayout";
+import CareersLayout from "./layouts/CareersLayout";
 
 //Pages
 import Faq from "./pages/help/Faq";
 import Contact from "./pages/help/Contact";
 import NotFound from "./pages/NotFound";
+import Careers, { careersLoader } from "./pages/careers/Careers";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,6 +26,9 @@ const router = createBrowserRouter(
       <Route path="help" element={<HelpLayout />}>
         <Route path="contact" element={<Contact />} />
         <Route path="faq" element={<Faq />} />
+      </Route>
+      <Route path="careers" element={<CareersLayout />}>
+        <Route index element={<Careers />} loader={careersLoader}></Route>
       </Route>
       <Route path="*" element={<NotFound />} />
     </Route>
